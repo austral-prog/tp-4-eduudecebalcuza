1f8534cad80a93286c6c6aabf6648a1c46f02207
eduudecebalcuza <162722197+eduudecebalcuza@users.noreply.github.com>
2024-04-18 22:08:26 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,9 @@
 def leap_year():
-    print("TO DO")
+    year1 = int(input('Ingrese su año:'))
+    if year1 % 4 == 0:
+        if year1 % 400 == 0: 
+            print(f'El año {year1} es bisiesto')
+        elif year1 % 400 != 0: 
+            print(f'El año {year1} no es bisiesto')
+    else:
+        print(f'El año {year1} no es bisiesto')
